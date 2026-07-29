1ced6b521a5da4b8c0925b2c2843970e5ebe6256
mehmeterencakmak <mehmeterncakmak@gmail.com>
2026-07-29 14:24:12 +0300

    Add NuGet configuration files and project assets for TodoList
    - Created TodoList.csproj.nuget.g.props to define NuGet properties.
    - Added TodoList.csproj.nuget.g.targets for build targets.
    - Generated project.assets.json to manage project dependencies and assets.
    - Included project.nuget.cache for caching NuGet package information.

---
+++ TodoList/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("[S]ee all TOTOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userInput = "A";
+Console.WriteLine(userInput);
+userInput = "ABC";
+Console.WriteLine(userInput);
+
+int number;
+number = 7;
+Console.WriteLine(number);
+
+int a = 1, b=2;
+string name, surname;
+
+Console.ReadKey();
+
+string @class = "First";

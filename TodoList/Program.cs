1e0f1557afb32cd39b443b8e8ec7d4bb773dd566
mehmeterencakmak <mehmeterncakmak@gmail.com>
2026-07-31 00:13:14 +0300

    update

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,21 +1,195 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("[S]ee all TOTOs");
-Console.WriteLine("[A]dd a TODO");
-Console.WriteLine("[R]emove a TODO");
-Console.WriteLine("[E]xit");
+﻿// Console.WriteLine("Hello, World!");
+// Console.WriteLine("[S]ee all TOTOs");
+// Console.WriteLine("[A]dd a TODO");
+// Console.WriteLine("[R]emove a TODO");
+// Console.WriteLine("[E]xit");
 
-string userInput = "A";
-Console.WriteLine(userInput);
-userInput = "ABC";
-Console.WriteLine(userInput);
+// var userChoice = Console.ReadLine();
 
-int number;
-number = 7;
-Console.WriteLine(number);
 
-int a = 1, b=2;
-string name, surname;
+// if (userChoice.Length <3 )
+// {
+//    Console.WriteLine("Short answer.");
+// }
+// else if(userChoice.Length <= 10)
+// {
+//     Console.WriteLine("Medium answer.");
+// }
 
-Console.ReadKey();
+// else
+// {
+//     Console.WriteLine("Long Answer");
+// }
+// Console.ReadKey();
 
-string @class = "First";
+// bool isUserInputAbc = userChoice == "ABC";
+// bool isUserInputNotAbc = userChoice != "ABC";
+// bool isUserInputNotAbc2 = !(userChoice == "ABC");
+
+
+// string userInput = "A";
+// Console.WriteLine(userInput);
+// userInput = "ABC";
+// Console.WriteLine(userInput);
+
+// int number;
+// number = 7;
+// Console.WriteLine(number);
+
+// int a = 1, b=2;
+// string name, surname;
+
+// Console.ReadKey();
+
+// string @class = "First";
+
+// int a = 10;
+// int b = 5;
+
+// Console.WriteLine(a + b);
+// Console.WriteLine(a - b);
+// Console.WriteLine(a * b);
+// Console.WriteLine(a / b);
+// Console.WriteLine("Mehmet" + " " + "Eren");
+
+
+// string word = "ABC";
+// int number = 5;
+
+// var worde = "AWEADS";
+// Console.WriteLine();
+
+// //scope
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("[S]ee all TOTOs");
+// Console.WriteLine("[A]dd a TODO");
+// Console.WriteLine("[R]emove a TODO");
+// Console.WriteLine("[E]xit");
+
+// var userChoice = Console.ReadLine();
+
+// if(userChoice.Length == 0)
+// {
+//     Console.WriteLine("Empty choice!");
+//     int number = 5;
+//     Console.WriteLine(number);
+// }
+// else
+// {
+//     Console.WriteLine("Non-empty choice: "+ userChoice) ;
+// }
+// Console.WriteLine("Your choice is: "+ userChoice);
+
+// Console.ReadKey();
+
+
+// // METHODS == function
+
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("[S]ee all TOTOs");
+// Console.WriteLine("[A]dd a TODO");
+// Console.WriteLine("[R]emove a TODO");
+// Console.WriteLine("[E]xit");
+
+// var userChoice = Console.ReadLine();
+
+// if( userChoice == "S")
+// {
+//     Console.WriteLine("Selected option: See all TODOs");
+// }
+// else if ( userChoice == "A" )
+// {
+//     PrintSelectedOption("See all TODOs");
+// }
+// else if(userChoice == "R")
+// {
+//     PrintSelectedOption("Remove a TODO");
+// }
+// else if ( userChoice == "E")
+// {
+//     PrintSelectedOption("Exit");
+// }
+
+// Console.ReadKey();
+
+// void PrintSelectedOption(string selectedOption)
+// {
+//     Console.WriteLine("Selected option: " + selectedOption);
+// }
+
+
+
+// // METHODS2 == function
+
+// using System.ComponentModel;
+
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("[S]ee all TOTOs");
+// Console.WriteLine("[A]dd a TODO");
+// Console.WriteLine("[R]emove a TODO");
+// Console.WriteLine("[E]xit");
+
+// var userChoice = Console.ReadLine();
+// bool isLong = IsLong(userChoice);
+
+// var result = Add(10,5);
+// Console.WriteLine("10 + 5 = " + result);
+
+// // if( userChoice == "S")
+// // {
+// //     Console.WriteLine("Selected option: See all TODOs");
+// // }
+// // else if ( userChoice == "A" )
+// // {
+// //     PrintSelectedOption("See all TODOs");
+// // }
+// // else if(userChoice == "R")
+// // {
+// //     PrintSelectedOption("Remove a TODO");
+// // }
+// // else if ( userChoice == "E")
+// // {
+// //     PrintSelectedOption("Exit");
+// // }
+
+// // Console.ReadKey();
+
+// // void PrintSelectedOption(string selectedOption)
+// // {
+// //     Console.WriteLine("Selected option: " + selectedOption);
+// // }
+
+
+// int Add (int a, int b)
+// {
+//     return a + b; 
+// }
+
+// bool IsLong(string input)
+// {
+//     return input.Length >10;
+     
+// }
+
+
+// // parse
+// Console.WriteLine("Provide a number");
+// string userInput = Console.ReadLine();
+// int number = int.Parse(userInput);
+// Console.WriteLine(number);
+
+
+// Console.WriteLine("Provide a number");
+// string userInput = Console.ReadLine();
+// int number = int.Parse(userInput);
+// Console.WriteLine(number);
+
+
+// // string interpolation
+// int a = 4, b = 2, c = 10;
+
+// Console.WriteLine("First is: " + a + ", second is: " + b + ",third is: " + c);
+
+// Console.WriteLine(
+//     $"First is: {a}, second is: {b}, third is:{c}"
+// );

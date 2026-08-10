6115438a9331d16a6bdb381e988f7c5f71188dd7
mehmeterencakmak <mehmeterncakmak@gmail.com>
2026-08-10 22:37:24 +0300

    inheritance

--- TodoList/OOP.cs
+++ TodoList/OOP.cs
@@ -421,13 +421,159 @@
 // }
 
 
-Console.WriteLine($"1 + 2 is {Calculator.Add(1, 2)}");
-Console.WriteLine($"1 - 2 is {Calculator.Substract(1, 2)}");
-Console.WriteLine($"1 * 2 is {Calculator.Multiply(1, 2)} ");
+// Console.WriteLine($"1 + 2 is {Calculator.Add(1, 2)}");
+// Console.WriteLine($"1 - 2 is {Calculator.Substract(1, 2)}");
+// Console.WriteLine($"1 * 2 is {Calculator.Multiply(1, 2)} ");
 
-static class Calculator
+// static class Calculator
+// {
+//     public static int Add(int a, int b) => a + b;
+//     public static int Substract(int a, int b) => a - b;
+//     public static int Multiply(int a, int b) => a * b;
+// }
+
+
+// Pizza application for  override, inheritence..
+
+var pizza = new Pizza();
+pizza.AddIngredient(new Cheddar());
+pizza.AddIngredient(new Mozzarella());
+pizza.AddIngredient(new TomataSauce());
+Console.WriteLine(pizza.Describe());
+
+
+// var ingredient = new Ingredient();
+// ingredient.PublicField = 10;
+
+// var cheddar = new Cheddar();
+// cheddar.PublicField = 20;
+
+// Console.WriteLine("Value in ingredient: " + ingredient.PublicField);
+// Console.WriteLine("Value in cheddar: " + cheddar.PublicField);
+
+// Console.WriteLine(cheddar.PublicMethod());
+// Console.WriteLine(cheddar.ProtectedMethod());
+// Console.WriteLine(cheddar.PrivateMethod());
+
+// Console.WriteLine("Variable of type Cheddar");
+// Cheddar cheddar = new Cheddar();
+// Console.WriteLine(cheddar.Name);
+
+// Console.WriteLine("Variable of type Ingredient");
+// Ingredient ingredient = new Cheddar();
+// Console.WriteLine(ingredient.Name);
+
+// var ingredients = new List<Ingredient>
+// {
+//     new Cheddar(),
+//     new Mozzarella(),
+//     new TomataSauce()
+// };
+// foreach(var ingredient in ingredients)
+// {
+//     Console.WriteLine(ingredient.Name);
+// }
+
+Console.ReadKey();
+
+public class Pizza
 {
-    public static int Add(int a, int b) => a + b;
-    public static int Substract(int a, int b) => a - b;
-    public static int Multiply(int a, int b) => a * b;
+    private List<Ingredient> _ingredients = new List<Ingredient>();
+
+    public void AddIngredient(Ingredient ingredient) => 
+    _ingredients.Add(ingredient);
+
+    public string Describe() => 
+        $"This is a pizza with {string.Join(", ", _ingredients)}";
 }
+public class Ingredient
+{   
+    public virtual string Name {get; } = "Some ingredient";
+    public int PublicField;
+    public string PublicMethod() => 
+        "This method is PUBLIC in the Ingredient class.";
+    protected string ProtectedMethod() => 
+        "This method is PROTECTED in the Ingredient class.";
+    private string PrivateMethod() => 
+        "This method is PRIVATE in the Ingredient class.";
+    
+}
+
+public class Cheese : Ingredient
+{
+    
+}
+
+public class ItalianFoodItem
+{
+    
+}
+
+public class Cheddar : Cheese
+{
+    public override string Name  => "Cheddar cheese";
+    public int AgedForMonths {get;}
+    public void UseMethodsFromBaseClass()
+    {
+        Console.WriteLine(PublicMethod());
+        Console.WriteLine(ProtectedMethod());
+        // Console.WriteLine(PrivateMethod());
+    }
+}
+public class TomataSauce : Cheese
+{
+    public  string Name  => "Tomato sauce";
+    public int TomatosIn100Grams {get;}
+}
+public class Mozzarella : Ingredient
+{
+    public override string Name  => "Mozzarella";
+    public bool IsLight {get;}
+}
+
+
+
+// var numbers = new List<int> {1, 4, 6, -1, 12, 44, -8, -19};
+// bool shallAddPositiveOnly = false;
+
+// NumbersSumCalculator calculator = 
+//     shallAddPositiveOnly ? 
+//     new PositiveNumbersSumCalculator() :
+//     new NumbersSumCalculator();
+
+// int sum = calculator.Calculate(numbers);
+
+
+ 
+// Console.WriteLine("Sum is: " +sum);
+// Console.ReadKey();
+
+
+// public class NumbersSumCalculator
+// {
+//     public int Calculate(List<int> numbers)
+//     {
+//         int sum = 0;
+//         foreach(int number in numbers)
+//         {
+//             if (ShallBeAdded(number))
+//             {
+//                 sum += number;
+//             }
+//         }
+//         return sum;
+//     }
+//     protected virtual bool ShallBeAdded(int number)
+//     {
+//         return true;
+//     }
+// }
+
+// public class PositiveNumbersSumCalculator 
+//     : NumbersSumCalculator
+// {
+//     protected override bool ShallBeAdded(int number)
+//     {
+//         return number > 0;
+//     }
+// }
